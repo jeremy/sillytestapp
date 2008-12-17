b4635abd18d4a54b753d8f845fba421611233e66
Jeremy Kemper <jeremy@bitsweat.net>
2008-12-17 13:16:47 -0800

    Update metal

--- app/metal/hello.rb
+++ app/metal/hello.rb
@@ -1,6 +1,13 @@
-class Hello < Rails::Rack::Metal
+require 'singleton'
+
+class Hello
+  include Singleton
   HELLO = [200, { "Content-Type" => "text/html" }, ["Hello World!"]]
   NOT_FOUND = [404, {}, []]
+
+  def self.call(env)
+    instance.call(env)
+  end
 
   def call(env)
     if env["PATH_INFO"] =~ /^\/hello$/
