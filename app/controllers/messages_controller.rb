5a7f48fad278d60d259b3ad70e5d20222968fa5d
Jeremy Kemper <jeremy@bitsweat.net>
2008-12-17 11:39:51 -0800

    Spacing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,17 @@
 class MessagesController < ApplicationController
   layout 'mr_layout', :only=>[:with_layout]
+
   def index
-    render :text=>"Hello World: #{Time.now.xmlschema}"
+    render :text => "Hello World: #{Time.now.xmlschema}"
   end
-  
+
   def with_template
   end
-  
+
   def with_layout
     render :action => "with_template"
   end
-  
+
   def silly_partials
     if num = params[:the_number]
       @the_number = num.to_i
